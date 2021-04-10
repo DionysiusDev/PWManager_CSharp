0952f8d4f18f0fcba92de448b4d2d504762d3594
Dion Helsby <dionhelsby@gmail.com>
2021-04-10 13:53:46 +1000

    updated existing form db connection

---
+++ PWManager/Details/Details.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PWManager.Details
+{
+    public partial class Details : Form
+    {
+        private long _lngPKID = 0;
+        private DataTable _dtbPassword = null;
+        bool _blnNew = false;
+        private SqlConnection sqlConnectionTool;
+
+        #region Constructors
+        public Details()
+        {
+            InitializeComponent();
+        }
+    
+        public Details(long PKID)
+        {
+            InitializeComponent();
+            _lngPKID = PKID;
+        }
+        #endregion
+
+        private void Details_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- PWManager/Options/ViewAll.cs
+++ PWManager/Options/ViewAll.cs
@@ -16,5 +16,48 @@ namespace PWManager.Options
         {
             InitializeComponent();
         }
+
+        #region Form Events
+        private void ViewAll_Load(object sender, EventArgs e)
+        {
+            PopulateGrid(); // populate grid
+        }
+        #endregion
+
+        #region Control Events
+        //  this loads the clicked cells password information in the password details screen
+        private void Password_DoubleClick(object sender, EventArgs e)
+        {
+            if (dgvPassword.CurrentCell == null) return;
+
+            long PKID = long.Parse(dgvPassword[0, dgvPassword.CurrentCell.RowIndex].Value.ToString());
+
+            Details.Details frm = new Details.Details(PKID);
+            if (frm.ShowDialog() == DialogResult.OK)
+            PopulateGrid();
+        }
+
+        #endregion
+
+        #region Helper Methods
+
+        /// <summary>
+        /// Method used for populating data to our data grid view.
+        /// </summary>
+        private void PopulateGrid()
+        {
+            PWManager_Model.DLL.PWManagerContext.ConnectionString = Properties.Settings.Default.ConnectionString;
+            DataTable dtbPassword = new DataTable();
+            dtbPassword = PWManager_Model.DLL.PWManagerContext.GetDataTable("PasswordInfo");
+            dgvPassword.DataSource = dtbPassword;
+
+            //added for removing columns from data table
+            //dtbPassword.Columns.Remove("PwId");
+
+            dgvPassword.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells;
+        }
+
+
+        #endregion
     }
 }
